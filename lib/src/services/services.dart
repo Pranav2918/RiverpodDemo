8931b1110b788166a2105e8aa068aab4a5fc3531
Pranav Dave <pranavdave18@gmail.com>
2025-02-28 16:52:23 +0530

    made service to fetch photos

--- lib/src/services/services.dart
+++ lib/src/services/services.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:riverpoddemo/src/data/models/photo.dart';
 
 import '../data/models/user.dart';
 
@@ -6,13 +7,25 @@ class ApiServices {
   final Dio _dio = Dio();
 
   Future<List<User>> fetchUsers() async {
-    try{
-      final response = await _dio.get("https://jsonplaceholder.typicode.com/users");
+    try {
+      final response =
+          await _dio.get("https://jsonplaceholder.typicode.com/users");
       List data = response.data as List;
       List<User> users = data.map((e) => User.fromJson(e)).toList();
       return users;
-    }catch (e){
+    } catch (e) {
       throw Exception("Something went wrong $e");
+    }
+  }
+
+  Future<List<Photo>> fetchPhotos() async {
+    try {
+      final response = await _dio.get("https://picsum.photos/v2/list");
+      List data = response.data as List;
+      print("DATA $data");
+      return data.map((e) => Photo.fromJson(e)).toList();
+    } catch (e) {
+      throw Exception("Something went wrong: $e");
     }
   }
 }
